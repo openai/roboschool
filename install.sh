b1b0d1f006e60a8c8c47253a7be182914fe85080
Peter Zhokhov <peterz@openai.com>
2018-12-06 11:04:49 -0800

    verbose installation of bullet

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ git clone https://github.com/olegklimov/bullet3 -b roboschool_self_collision .
 mkdir build && cd build
 cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$ROBOSCHOOL_PATH/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
 make -j4
-make install > /dev/null
+make install
 
 ls $CPP_HOUSEHOLD/bullet_local_install/lib
 
