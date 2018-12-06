f4281a524cfc1bec58897f279b31c7ef9e63bbbc
Peter Zhokhov <peterz@openai.com>
2018-12-05 18:06:21 -0800

    move code tweaking before call to configure

--- install.sh
+++ install.sh
@@ -14,14 +14,16 @@ mkdir -p $QT5_SRCDIR && cd $QT5_SRCDIR
 if [ ! -f $QT_SRC ]; then curl -OL http://download.qt.io/archive/qt/5.6/5.6.0/single/$QT_SRC; fi
 tar -xf $QT_SRC
 cd ${QT_SRC%.tar.gz}
-./configure -opensource -confirm-license -prefix $CPP_HOUSEHOLD/qt5_local_install -no-xcb -no-openssl -widgets -opengl -make libs
 
-# remove reference to signalfd.h not present on CentOS 5
+# Tweak Qt code a little bit (build errors without these):
+# 1. remove reference to signalfd.h not present on CentOS 5
 sed -i '/signalfd/d' qtbase/src/platformsupport/fbconvenience/qfbvthandler.cpp
-# remove reference to asm/byteorder.h - present, but fails to compile with --std=c++0x
+# 2. remove reference to asm/byteorder.h - present, but fails to compile with --std=c++0x
 sed -i '/byteorder/d' qtbase/src/testlib/3rdparty/linux_perf_event_p.h
-# add legacy glx flag to the compiler
+# 3. add legacy glx flag to the compiler
 echo "QMAKE_CXX_FLAGS += -DGLX_GLXEXT_LEGACY" >> qtbase/src/plugins/platforms/offscreen/offscreen.pro
+
+./configure -opensource -confirm-license -prefix $CPP_HOUSEHOLD/qt5_local_install -no-xcb -no-openssl -widgets -opengl -make libs
 
 make -j4 > /dev/null
 make install > /dev/null
