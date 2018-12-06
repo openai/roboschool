770f7474d95be70b2af7cf1d50283398eadd8c1b
Peter Zhokhov <peterz@openai.com>
2018-12-05 18:40:38 -0800

    only do code tweaks in linux

--- install.sh
+++ install.sh
@@ -15,13 +15,15 @@ if [ ! -f $QT_SRC ]; then curl -OL http://download.qt.io/archive/qt/5.6/5.6.0/si
 tar -xf $QT_SRC
 cd ${QT_SRC%.tar.gz}
 
-# Tweak Qt code a little bit (build errors without these):
-# 1. remove reference to signalfd.h not present on CentOS 5
-sed -i '/signalfd/d' qtbase/src/platformsupport/fbconvenience/qfbvthandler.cpp
-# 2. remove reference to asm/byteorder.h - present, but fails to compile with --std=c++0x
-sed -i '/byteorder/d' qtbase/src/testlib/3rdparty/linux_perf_event_p.h
-# 3. add legacy glx flag to the compiler
-echo "QMAKE_CXXFLAGS += -DGLX_GLXEXT_LEGACY" >> qtbase/src/plugins/platforms/offscreen/offscreen.pro
+if [ $(uname) == 'Linux' ]; then
+    # Tweak Qt code a little bit (build errors without these):
+    # 1. remove reference to signalfd.h not present on CentOS 5
+    sed -i '/signalfd/d' qtbase/src/platformsupport/fbconvenience/qfbvthandler.cpp
+    # 2. remove reference to asm/byteorder.h - present, but fails to compile with --std=c++0x
+    sed -i '/byteorder/d' qtbase/src/testlib/3rdparty/linux_perf_event_p.h
+    # 3. add legacy glx flag to the compiler
+    echo "QMAKE_CXXFLAGS += -DGLX_GLXEXT_LEGACY" >> qtbase/src/plugins/platforms/offscreen/offscreen.pro
+fi
 
 ./configure -opensource -confirm-license -prefix $CPP_HOUSEHOLD/qt5_local_install -widgets -opengl -make libs \
              -no-gstreamer -no-pulseaudio -no-alsa \
