117386bfbd37e47ebfaf3505ced1aabd9d913de6
Peter Zhokhov <peterz@openai.com>
2018-12-10 19:32:09 -0800

    add ld_library_path to help auditwheel find second-order libraries

--- exports.sh
+++ exports.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+cd $(dirname "$0")
+ROBOSCHOOL_PATH=$(pwd)
+CPP_HOUSEHOLD=$ROBOSCHOOL_PATH/roboschool/cpp-household
+
 PYTHON_BIN=$(readlink -f $(which python))
 PYTHON_ROOT=${PYTHON_BIN%/bin/python*}
 PYTHON_VER=${PYTHON_BIN#$PYTHON_ROOT/bin/python}
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${PYTHON_ROOT}/lib/pkgconfig
 export CPATH=$CPATH:${PYTHON_ROOT}/include/python${PYTHON_VER}m
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CPP_HOUSEHOLD/bullet_local_install/lib
 
