6aa94670d60b066dba5cde6cad5149d27596b90a
Peter Zhokhov <peterz@openai.com>
2018-12-11 11:45:33 -0800

    disable python3.7 build - problems with building boost; also use new binaries for qt5

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cd $(dirname "$0")
 pip install cmake
 
 if [ $(uname) == 'Linux' ]; then
-  QT5_BIN=qt5.6.0_centos5_2018-12-06a.tar.gz
+  QT5_BIN=qt5.6.0_centos5_2018-12-11.tar.gz
 fi
 
 cd $CPP_HOUSEHOLD
@@ -39,7 +39,6 @@ BULLET_SRCDIR=$TMPDIR/bullet3
 rm -rf $BULLET_SRCDIR
 mkdir -p $BULLET_SRCDIR && cd $BULLET_SRCDIR
 git clone https://github.com/olegklimov/bullet3 -b roboschool_self_collision .
-echo 2.8.7 > VERSION
 mkdir build && cd build
 cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$ROBOSCHOOL_PATH/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
 make -j4 > $TMPDIR/bullet_make.log || tail -100 $TMPDIR/bullet_make.log
