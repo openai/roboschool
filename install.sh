e6e9b5d1178bfe7092a536211d63424e25ed2ef4
Peter Zhokhov <peterz@openai.com>
2018-12-12 15:57:06 -0800

    try CMP0042 OLD cmake policy to fix linking the bullet dylibs on osx; bump version

--- install.sh
+++ install.sh
@@ -38,13 +38,15 @@ if [ $(uname) == 'Darwin' ]; then
     brew install qt boost-python3 assimp
     pkg-config --libs Qt5Widgets Qt5OpenGL
     ls /usr/local/opt/qt/lib
-    # find / -name "python*.pc"
 fi
 
 BULLET_SRCDIR=$TMPDIR/bullet3
 rm -rf $BULLET_SRCDIR
 mkdir -p $BULLET_SRCDIR && cd $BULLET_SRCDIR
 git clone https://github.com/olegklimov/bullet3 -b roboschool_self_collision .
+
+if [ $(uname) == 'Darwin' ]; then sed -i '' 's/SET CMP0042 NEW/SET CMP0042 OLD/g' CMakeLists.txt; fi
+
 mkdir build && cd build
 cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$ROBOSCHOOL_PATH/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
 make -j4 > $TMPDIR/bullet_make.log || tail -100 $TMPDIR/bullet_make.log
