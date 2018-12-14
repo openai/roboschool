3d40133c2c8e130c153a5350aeb44c57f51bcfc5
Peter Zhokhov <peterz@openai.com>
2018-12-14 14:02:10 -0800

    fix imports in osx_test_wheel.sh

--- osx_test_wheel.sh
+++ osx_test_wheel.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -ex
+. multibuild/common_utils.sh
+. multibuild/travis_steps.sh
+before_install
+
 pip install wheelhouse/roboschool*.whl
 
 python -c "import roboschool, gym; gym.make('RoboschoolAnt-v1')"
