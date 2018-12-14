1630cfc326f1c5afebf2ade4d8cb743686115d97
Peter Zhokhov <peterz@openai.com>
2018-12-13 16:35:03 -0800

    try gcs upload (on every build for now)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 set -ex
+cat $GCS_CREDS | base64 --decode > .gcs_creds.json
+export GOOGLE_APPLICATION_CREDENTIALS=$PWD/.gcs_creds.json
+gsutil cp wheelhouse/* gs://games-wheels/roboschool/
 if [[ ! -z "$TRAVIS_TAG" ]]; then
-    pip install twine
-    twine upload wheelhouse/*
+    # pip install twine
+    # twine upload wheelhouse/*
 
     if [[ ! -z "$DEPLOY_SDIST" ]]; then
         python setup.py sdist
