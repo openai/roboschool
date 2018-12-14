948668d0ebfd5b2e4607eb06528ccdbdfa22349a
Peter Zhokhov <peterz@openai.com>
2018-12-14 11:51:32 -0800

    bash syntax is unforgiving

--- build_wheel.sh
+++ build_wheel.sh
@@ -5,6 +5,6 @@ if [ $(uname) == 'Linux' ]; then
     docker run -v $PWD:/io python:${MB_PYTHON_VERSION} /io/docker_build_wheel.sh
 fi
 
-if [ $(uname) == 'Darwin' ]; then;
+if [ $(uname) == 'Darwin' ]; then
     ./osx_build_wheel.sh
 fi
