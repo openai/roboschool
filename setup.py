f4f2e224608e6fb225e5b777cf0e8035abf74b07
Peter Zhokhov <peterz@openai.com>
2018-12-14 16:50:47 -0800

    bump version to 1.0.11

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ for n in need_files: print("-- %s" % n)
 
 setup(
     name = 'roboschool',
-    version = '1.0.10',
+    version = '1.0.11',
     description = 'OpenAI Household Simulator: mobile manipulation using Bullet',
     maintainer = 'Oleg Klimov',
     maintainer_email = 'omgtech@gmail.com',
