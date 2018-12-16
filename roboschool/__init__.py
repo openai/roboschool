baf6850b2e233c06d4b044f6f26100a474682352
Peter Zhokhov <peterz@openai.com>
2018-12-16 13:20:34 -0800

    copy plugins

--- roboschool/__init__.py
+++ roboschool/__init__.py
@@ -3,7 +3,7 @@ from gym.envs.registration import register
 
 import os
 import os.path as osp
-os.environ['QT_PLUGIN_PATH'] = osp.join(osp.dirname(osp.abspath(__file__)), 'cpp-household/qt5_local_install/plugins')
+os.environ['QT_PLUGIN_PATH'] = osp.join(osp.dirname(osp.abspath(__file__)), '.libs/qt_plugins')
 
 register(
     id='RoboschoolInvertedPendulum-v1',
