c6a29984aee1cb4fe6419aa6ab6052662f83361d
Peter Zhokhov <peterz@openai.com>
2018-12-16 11:25:18 -0800

    fix second-order grafting

--- roboschool_compile_and_graft.sh
+++ roboschool_compile_and_graft.sh
@@ -1,37 +1,65 @@
 #!/bin/bash
 set -e
 
-
-
 function osx_graft_lib {
     libfile=$1
-    deps=$(otool -L $libfile | awk '{print $1}')
-    deppattern=$2
-    new_libdir=.libs
-    mkdir -p $new_libdir
+    libdir=$(dirname $libfile)
+    cwd=$(pwd)
+    deps=$(otool -L $libfile | awk 'FNR>2 {print $1}')
+    
+#    patterns=${@:3}
+    graft_dir=$2
+    # extra_path=$(dirname $libfile)
+    mkdir -p $graft_dir
+
+#    echo "processing $libfile"
+#     echo "Dependency list:"
+#     for dep in $deps; do
+#         echo "$dep"
+#     done
+# 
+#     echo "Pattern list:"
+#     for deppattern in $patterns; do
+#         echo "$deppattern"
+#     done;
+
     for dep in $deps; do
-        if [[ "$dep" =~ $deppattern ]]; then
-            new_libpath="$new_libdir/${dep##*/}"
-            new_dep="@loader_path/$new_libpath"
-            echo "$dep found, grafting to $new_libpath ($new_dep)"
-            cp $dep $new_libdir/${dep##*/}
-            install_name_tool -change $dep $new_dep $libfile
-        fi
-    done    
+        echo $dep
+        patterns=${@:3}
+        for deppattern in $patterns; do
+            if [[ "$dep" =~ $deppattern ]]; then
+                new_depname=${dep##*/}
+                new_deppath="$graft_dir/$new_depname"
+                rel_path=$(realpath --relative-to="$libdir" $new_deppath) 
+                new_dep="@loader_path/$rel_path"
+                echo "$libfile depends on $dep, relinking to $new_deppath ($new_dep)"
+                install_name_tool -change $dep $new_dep $libfile
+                if [ ! -f $new_deppath ]; then
+                    echo "$new_deppath not found, copying and calling self" 
+                    cp $dep $new_deppath
+                    chmod 777 $new_deppath
+                    osx_graft_lib $new_deppath $graft_dir $patterns
+                    echo "Finished recursive call"
+                else
+                    echo "grafted library $new_deppath already exists"
+                fi
+            fi
+        done    
+    done
 }
 
 cd $(dirname "$0")
 
 cd roboschool/cpp-household
-make clean && make -j4
+make clean
+make -j4
 cd ..
 
 if [ $(uname) == 'Darwin' ]; then
-    osx_graft_lib cpp_household.so .+/Python
-    osx_graft_lib cpp_household.so .+/libboost_python.*\.dylib
-    osx_graft_lib cpp_household.so .+/QtCore
-    osx_graft_lib cpp_household.so .+/QtGui
-    osx_graft_lib cpp_household.so .+/QtWidgets
-    osx_graft_lib cpp_household.so .+/QtOpenGL
-    osx_graft_lib cpp_household.so .+/libassimp.*\.dylib
+    osx_graft_lib cpp_household.so .libs ^/.+/Python
+    osx_graft_lib cpp_household.so .libs ^/.+/libboost_python.*\.dylib
+    osx_graft_lib cpp_household.so .libs ^/.+/QtCore ^/.+/QtGui ^/.+/QtWidgets ^/.+/QtOpenGL
+    # HACK - bash had problems with returning from recursion properly
+    osx_graft_lib cpp_household.so .libs ^/.+/QtCore ^/.+/QtGui ^/.+/QtWidgets ^/.+/QtOpenGL
+    osx_graft_lib cpp_household.so .libs ^/.+/libassimp.*\.dylib
 fi
