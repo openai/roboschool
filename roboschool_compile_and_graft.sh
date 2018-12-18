c4d8a0b9cd4fbe0b1adbdee0c483355cbac74b0f
Peter Zhokhov <peterz@openai.com>
2018-12-17 21:11:51 -0800

    bump version; single call to graft_libs

--- roboschool_compile_and_graft.sh
+++ roboschool_compile_and_graft.sh
@@ -62,10 +62,7 @@ make -j4
 cd ..
 
 
-graft_libs cpp_household.so .libs ^/.+/libboost_python.+
-graft_libs cpp_household.so .libs ^/.+Qt.+
-graft_libs cpp_household.so .libs ^/.+/libassimp.+
-graft_libs cpp_household.so .libs ^/.+/libLinearMath.+ ^/.+/libBullet.+ ^/.+/libPhysicsClientC_API.+
+graft_libs cpp_household.so .libs ^/.+/libboost_python.+ ^/.+Qt.+ ^/.+/libassimp.+ ^/.+/libLinearMath.+ ^/.+/libBullet.+ ^/.+/libPhysicsClientC_API.+
 
 if [ $(uname) == 'Darwin' ]; then
     # HACK - this should auto-detect plugins dir
