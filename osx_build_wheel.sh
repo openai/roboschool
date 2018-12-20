4b2b3c5863c85c66827664ab98c5a6862684e689
Peter Zhokhov <peterz@openai.com>
2018-12-19 16:13:41 -0800

    ignore brew return code

--- osx_build_wheel.sh
+++ osx_build_wheel.sh
@@ -5,7 +5,8 @@ git clone https://github.com/matthew-brett/multibuild && cd multibuild && git ch
 . multibuild/travis_steps.sh
 before_install
 
-brew install qt assimp boost-python3
+brew install qt assimp boost-python3 || echo "warning - brew returned non-zero exit code. \nThis may be a sign of error, but may also be because brew wants to point something out. Please inspect logs above"
+
 . ./exports.sh
 ./install_bullet.sh
 ./roboschool_compile_and_graft.sh
