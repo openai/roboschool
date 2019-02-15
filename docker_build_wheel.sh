e9eefb057800b73078d60d3e8697e9c18f7956e9
peter <peterzhokhoff@gmail.com>
2019-02-14 17:57:52 -0800

    try building with standard boost-python

--- docker_build_wheel.sh
+++ docker_build_wheel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ex
-apt-get update && apt-get install -y libgl1-mesa-dev qtbase5-dev libqt5opengl5-dev libassimp-dev patchelf cmake > /dev/null
+apt-get update && apt-get install -y libgl1-mesa-dev qtbase5-dev libqt5opengl5-dev libassimp-dev patchelf cmake libboost-python-dev> /dev/null
 
 cd $(dirname "$0")
 . ./exports.sh
 
 ./install_boost.sh
-./install_bullet.sh
+# ./install_bullet.sh
 ./roboschool_compile_and_graft.sh
 
 pip wheel --no-deps -w /io/wheelhouse .
