dcd7ba7c6940083a8200e24510f4b10bc92bbd90
Peter Zhokhov <peterz@openai.com>
2019-02-15 09:58:23 -0800

    try with system-level python_boost

--- docker_build_wheel.sh
+++ docker_build_wheel.sh
@@ -5,8 +5,8 @@ apt-get update && apt-get install -y libgl1-mesa-dev qtbase5-dev libqt5opengl5-d
 cd $(dirname "$0")
 . ./exports.sh
 
-./install_boost.sh
-# ./install_bullet.sh
+# ./install_boost.sh
+./install_bullet.sh
 ./roboschool_compile_and_graft.sh
 
 pip wheel --no-deps -w /io/wheelhouse .
