2a4ee34811f2a431c5370c9e22ec13c7541e1232
Peter Zhokhov <peterz@openai.com>
2019-02-15 19:50:06 -0800

    install_boost works with python3

--- install_boost.sh
+++ install_boost.sh
@@ -7,6 +7,6 @@ if [ $(uname) == 'Linux' ]; then
     tar -xf boost_1_58_0.tar.bz2
     cd boost_1_58_0
 
-     ./bootstrap.sh --with-python=$(which python) --with-libraries=python --prefix=$HOME/.boost --with-python-root=$PYTHON_ROOT
+     ./bootstrap.sh --with-python=$(which python3) --with-libraries=python --prefix=$HOME/.boost --with-python-root=$PYTHON_ROOT
      ./b2 install > $BOOST_SRCDIR/boost_make.log || (tail -100 $BOOST_SRCDIR/boost_make.log; exit 1)
 fi
