4aae3ced9b41c7932c778ac32eafc3b1727d031f
Peter Zhokhov <peterz@openai.com>
2019-03-12 17:06:15 -0700

    fix typo in install_boost.sh

--- install_boost.sh
+++ install_boost.sh
@@ -5,7 +5,7 @@ if [ $(uname) == 'Linux' ]; then
     mkdir -p $BOOST_SRCDIR && cd $BOOST_SRCDIR
     # curl -OL https://storage.googleapis.com/games-src/boost/boost_1_58_0.tar.bz2
     curl -OL https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
-    tar -xf boost_1_69_0.tar.bz2
+    tar -xf boost_1_69_0.tar.gz
     cd boost_1_69_0
 
      ./bootstrap.sh --with-python=$(which python) --with-libraries=python --prefix=$HOME/.boost --with-python-root=$PYTHON_ROOT
