17d48feddd8540b3c6a3b96ef221066d02ed56d7
Peter Zhokhov <peterz@openai.com>
2019-04-04 16:39:06 -0700

    make render method work even without 'close' argument

--- roboschool/gym_mujoco_xml_env.py
+++ roboschool/gym_mujoco_xml_env.py
@@ -74,7 +74,7 @@ class RoboschoolMujocoXmlEnv(gym.Env):
         self.camera = self.scene.cpp_world.new_camera_free_float(self.VIDEO_W, self.VIDEO_H, "video_camera")
         return s
 
-    def render(self, mode, close):
+    def render(self, mode='human', close=False):
         if close:
             return
         if mode=="human":
